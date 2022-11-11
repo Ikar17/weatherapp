64924ece20474dce2e05393283af0627e35b4190
Przemek <ikaria@poczta.onet.pl>
2022-11-11 21:37:19 +0100

    Changed url in APIservice.js

--- src/apiService.js
+++ src/apiService.js
@@ -2,7 +2,7 @@ export const getWeather = (city) => {
     const apiKey = "cbe1dc102e94da9d885a89a927b38bce";
     const limit = "1";
 
-    return fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=${limit}&appid=${apiKey}`)
+    return fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=${limit}&appid=${apiKey}`)
     .then((res) => res.json())
     .then((localizations) => {
         const lat = localizations[0].lat;
