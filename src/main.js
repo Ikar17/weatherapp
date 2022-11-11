0e223fcda7c95c0b7d0ec873a6c5c423f3231893
Przemek <ikaria@poczta.onet.pl>
2022-11-11 15:36:46 +0100

    Added fading in/out and switching views

--- src/main.js
+++ src/main.js
@@ -14,6 +14,8 @@ const connectHTMLElems = () =>{
     viewElems.searchButton = getDOMElems("searchButton");
 
     viewElems.weatherForecastView = getDOMElems("weatherForecastView");
+    viewElems.returnButton = getDOMElems("returnButton");
+
     viewElems.weatherInfoCity = getDOMElems("weatherInfoCity");
     viewElems.weatherInfoTemperature = getDOMElems("weatherInfoTemperature");
 }
@@ -21,6 +23,7 @@ const connectHTMLElems = () =>{
 const setupListeners = () => {
     viewElems.searchInput.addEventListener("keydown", onEnterSubmit);
     viewElems.searchButton.addEventListener("click", onClickSubmit);
+    viewElems.returnButton.addEventListener("click", returnToSearch);
 }
 
 const initializeApp = () =>{
@@ -30,17 +33,49 @@ const initializeApp = () =>{
 
 const onEnterSubmit = (event) => {
     if(event.key === 'Enter'){
+        fadeInOut();
         const value = viewElems.searchInput.value;
         getWeather(value).then(data => {
             console.log(data);
+            switchView();
+            fadeInOut();
         });
     }
 };
 const onClickSubmit = (event) => {
+    fadeInOut();
     const value = viewElems.searchInput.value;
     getWeather(value).then(data => {
         console.log(data);
+        switchView();
+        fadeInOut();
     });
 };
+
+const fadeInOut = () => {
+    if(viewElems.mainContainer.style.opacity==='1' || viewElems.mainContainer.style.opacity === '' ){
+        viewElems.mainContainer.style.opacity = '0';
+    }else{
+        viewElems.mainContainer.style.opacity = '1';
+    }
+}
+
+const switchView = () => {
+    if(viewElems.weatherSearchView.style.display !== 'none'){
+        viewElems.weatherSearchView.style.display = 'none';
+        viewElems.weatherForecastView.style.display = 'flex';
+    }else{
+        viewElems.weatherForecastView.style.display = 'none';
+        viewElems.weatherSearchView.style.display = 'flex';
+    }
+}
+
+const returnToSearch = () => {
+    fadeInOut();
+    setTimeout(()=>{
+        switchView();
+        fadeInOut();
+    },500);
+}
 
 document.addEventListener("DOMContentLoaded", initializeApp);
