270b8cae2df599fcc358ce686ed7598877eebac8
Przemek <ikaria@poczta.onet.pl>
2022-11-11 10:14:58 +0100

    Setup connection with DOM elements and added event listeners

--- src/main.js
+++ src/main.js
@@ -1,3 +1,33 @@
 
+let viewElems = {};
 
-document.addEventListener("DOMContentLoaded", initial);
+const getDOMElems = (id) => {
+    return document.getElementById(id);
+}
+
+const connectHTMLElems = () =>{
+    viewElems.mainContainer = getDOMElems("mainContainer");
+    viewElems.weatherSearchView = getDOMElems("weatherSearchView");
+
+    viewElems.searchInput = getDOMElems("searchInput");
+    viewElems.searchButton = getDOMElems("searchButton");
+
+    viewElems.weatherForecastView = getDOMElems("weatherForecastView");
+    viewElems.weatherInfoCity = getDOMElems("weatherInfoCity");
+    viewElems.weatherInfoTemperature = getDOMElems("weatherInfoTemperature");
+}
+
+const setupListeners = () => {
+    viewElems.searchInput.addEventListener("keydown", onEnterSubmit);
+    viewElems.searchButton.addEventListener("click", onClickSubmit);
+}
+
+const initializeApp = () =>{
+    connectHTMLElems();
+    setupListeners();
+}
+
+const onEnterSubmit = () => {};
+const onClickSubmit = () => {};
+
+document.addEventListener("DOMContentLoaded", initializeApp);
